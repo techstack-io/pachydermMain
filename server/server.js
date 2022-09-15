280207121a6879790bf9f5c4c293d9a2c3964595
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-09-14 22:51:30 -0500

    added backend and installed dependancies

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import cors from 'cors'
+// Import reqire
+const morgan = require('morgan');
+// Import require
+require('dotenv').config();
+
+// Create Express App
+const app = express();
