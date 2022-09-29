e6dba883bd840f81f13e9b8d266528920e19a2d2
Dan <support@stackmetric.io>
2022-09-29 15:26:34 -0500

    Finished CSRF

--- client/context/index.js
+++ client/context/index.js
@@ -64,6 +64,14 @@ const Provider = ({ children }) => {
     }
   );
 
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get("/api/csrf-token");
+      axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+    };
+    getCsrfToken();
+  }, []);
+
   return (
     <Context.Provider value={{ state, dispatch }}>
         {children}
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import { readdirSync } from "fs";
 import mongoose from 'mongoose';
-import csrf from 'csrf';
+import csrf from 'csurf';
+import cookieParser from 'cookie-parser';
 
 const csrfProtection = csrf({
   cookie: true
@@ -19,6 +20,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 // Connect database
 mongoose
@@ -28,6 +30,15 @@ mongoose
 
 // route
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+// CSRF
+app.use(csrfProtection)
+
+app.get('/api/csrf-token', (req, res) => {
+  res.json({
+    csrfToken: req.csrfToken()
+  });
+})
 
 // port
 const port = process.env.PORT || 8000;
